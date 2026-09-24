66713938c545895c5a1aecf733fb5520244b7ed2
MD.Ferdous <75322918+ferdousr3@users.noreply.github.com>
2026-09-24 13:33:57 +0600

    fix: update routing and asset paths for consistency (#21)

--- apps/mattrika-web/src/app/app.routes.ts
+++ apps/mattrika-web/src/app/app.routes.ts
@@ -3,11 +3,7 @@ import { Route } from '@angular/router'
 export const appRoutes: Route[] = [
     {
         path: '',
-        redirectTo: 'home',
         pathMatch: 'full',
-    },
-    {
-        path: 'home',
         loadComponent: () =>
             import('./pages/landing/landing.component').then(
                 (m) => m.LandingComponent,
