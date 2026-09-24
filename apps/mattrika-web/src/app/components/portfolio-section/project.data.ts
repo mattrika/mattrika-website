80ed2df7f924cea35e24148252fbdb650fb1e778
ferdousr3 <ferdousr1992@gmail.com>
2026-09-24 13:47:53 +0600

    bug: fix all image path

--- apps/mattrika-web/src/app/components/portfolio-section/project.data.ts
+++ apps/mattrika-web/src/app/components/portfolio-section/project.data.ts
@@ -25,14 +25,14 @@ export const projects: Project[] = [
     {
         title: 'Lieblingsmode',
         category: 'Ecommerce',
-        thumbnail: 'projects/Lieblings-Mode/main.png',
+        thumbnail: '/projects/Lieblings-Mode/main.png',
         images: [
-            'projects/Lieblings-Mode/main.png',
-            'projects/Lieblings-Mode/product-category.png',
-            'projects/Lieblings-Mode/product-design.png',
-            'projects/Lieblings-Mode/product-design-custom-image.png',
-            'projects/Lieblings-Mode/single-product.png',
-            'projects/Lieblings-Mode/terndig-product.png',
+            '/projects/Lieblings-Mode/main.png',
+            '/projects/Lieblings-Mode/product-category.png',
+            '/projects/Lieblings-Mode/product-design.png',
+            '/projects/Lieblings-Mode/product-design-custom-image.png',
+            '/projects/Lieblings-Mode/single-product.png',
+            '/projects/Lieblings-Mode/terndig-product.png',
         ],
         summary: 'Custom Fashion Design & E-Commerce Platform',
         description:
@@ -46,11 +46,11 @@ export const projects: Project[] = [
     {
         title: 'Montuams Auto',
         category: 'Automotive',
-        thumbnail: 'projects/Montuams/main.png',
+        thumbnail: '/projects/Montuams/main.png',
         images: [
-            'projects/Montuams/main.png',
-            'projects/Montuams/service-deal.png',
-            'projects/Montuams/appoinments-review.png',
+            '/projects/Montuams/main.png',
+            '/projects/Montuams/service-deal.png',
+            '/projects/Montuams/appoinments-review.png',
         ],
         summary: 'Professional Auto Repair and Maintenance Services',
         description:
@@ -63,15 +63,15 @@ export const projects: Project[] = [
     {
         title: 'ContentERP',
         category: 'Enterprise',
-        thumbnail: 'projects/ContentERP/main.png',
+        thumbnail: '/projects/ContentERP/main.png',
         images: [
-            'projects/ContentERP/main.png',
-            'projects/ContentERP/Dashboard.png',
-            'projects/ContentERP/Dashboard-Article-Editor.png',
-            'projects/ContentERP/Dashboard-Contents.png',
-            'projects/ContentERP/Dashboard-Queue.png',
-            'projects/ContentERP/Dashboard-Chat.png',
-            'projects/ContentERP/Dashboard-Social.png',
+            '/projects/ContentERP/main.png',
+            '/projects/ContentERP/Dashboard.png',
+            '/projects/ContentERP/Dashboard-Article-Editor.png',
+            '/projects/ContentERP/Dashboard-Contents.png',
+            '/projects/ContentERP/Dashboard-Queue.png',
+            '/projects/ContentERP/Dashboard-Chat.png',
+            '/projects/ContentERP/Dashboard-Social.png',
         ],
         summary: 'Smart Content Workflow Platform',
         description:
@@ -85,12 +85,12 @@ export const projects: Project[] = [
     {
         title: 'Pagung Construction',
         category: 'Construction',
-        thumbnail: 'projects/Pagung/main.png',
+        thumbnail: '/projects/Pagung/main.png',
         images: [
-            'projects/Pagung/main.png',
-            'projects/Pagung/service.png',
-            'projects/Pagung/work.png',
-            'projects/Pagung/appoinment.png',
+            '/projects/Pagung/main.png',
+            '/projects/Pagung/service.png',
+            '/projects/Pagung/work.png',
+            '/projects/Pagung/appoinment.png',
         ],
         summary: 'Construction & Remodeling Services Landing Page',
         description:
@@ -103,14 +103,14 @@ export const projects: Project[] = [
     {
         title: 'Quizzaro',
         category: 'Entertainment',
-        thumbnail: 'projects/Quizzaro/main.png',
+        thumbnail: '/projects/Quizzaro/main.png',
         images: [
-            'projects/Quizzaro/main.png',
-            'projects/Quizzaro/quiz.png',
-            'projects/Quizzaro/game-list.png',
-            'projects/Quizzaro/rank.png',
-            'projects/Quizzaro/rank-list.png',
-            'projects/Quizzaro/knowladge.png',
+            '/projects/Quizzaro/main.png',
+            '/projects/Quizzaro/quiz.png',
+            '/projects/Quizzaro/game-list.png',
+            '/projects/Quizzaro/rank.png',
+            '/projects/Quizzaro/rank-list.png',
+            '/projects/Quizzaro/knowladge.png',
         ],
         summary: 'Play, Learn & Win Quiz Platform',
         description:
@@ -124,8 +124,8 @@ export const projects: Project[] = [
     {
         title: 'Shared Auth',
         category: 'Enterprise',
-        thumbnail: 'projects/Shared-Auth/main.png',
-        images: ['projects/Shared-Auth/main.png'],
+        thumbnail: '/projects/Shared-Auth/main.png',
+        images: ['/projects/Shared-Auth/main.png'],
         summary: 'Enterprise Identity Management System',
         description:
             'A centralized authorization engine built to manage hierarchical organizational structures. It facilitates secure user onboarding, organization-specific group creation, and granular Role-Based Access Control (RBAC). The system enables administrators to define custom permissions and manage user access levels within specific organization and group boundaries.',
@@ -137,8 +137,8 @@ export const projects: Project[] = [
     {
         title: 'PdfAir',
         category: 'Tools',
-        thumbnail: 'projects/PdfAir/main.png',
-        images: ['projects/PdfAir/main.png'],
+        thumbnail: '/projects/PdfAir/main.png',
+        images: ['/projects/PdfAir/main.png'],
         summary: 'Manage and Sync Your PDF Library Across Devices',
         description:
             'You have always wanted to read the books in your collection. You see interesting artictes online and want to save it as PDF to read it later. But you dont have enough time. Also, you forget where you stored them.',
@@ -151,12 +151,12 @@ export const projects: Project[] = [
     {
         title: 'Michrobux',
         category: 'Ecommerce',
-        thumbnail: 'projects/Michrobux/main.png',
+        thumbnail: '/projects/Michrobux/main.png',
         images: [
-            'projects/Michrobux/main.png',
-            'projects/Michrobux/hot-products.png',
-            'projects/Michrobux/product-details.png',
-            'projects/Michrobux/player-says.png',
+            '/projects/Michrobux/main.png',
+            '/projects/Michrobux/hot-products.png',
+            '/projects/Michrobux/product-details.png',
+            '/projects/Michrobux/player-says.png',
         ],
         summary: 'Level Up Your Roblox Experience',
         description:
@@ -169,11 +169,11 @@ export const projects: Project[] = [
     {
         title: 'Al-Firdaous',
         category: 'Community',
-        thumbnail: 'projects/Mosque/main.png',
+        thumbnail: '/projects/Mosque/main.png',
         images: [
-            'projects/Mosque/main.png',
-            'projects/Mosque/seeking-clarity-in-islam.png',
-            'projects/Mosque/upcomming-events.png',
+            '/projects/Mosque/main.png',
+            '/projects/Mosque/seeking-clarity-in-islam.png',
+            '/projects/Mosque/upcomming-events.png',
         ],
         summary: 'Comprehensive Mosque Management & Community Platform',
         description:
@@ -186,13 +186,13 @@ export const projects: Project[] = [
     {
         title: 'Mr. Tasty',
         category: 'Restaurant',
-        thumbnail: 'projects/Mrtasty/main.png',
+        thumbnail: '/projects/Mrtasty/main.png',
         images: [
-            'projects/Mrtasty/main.png',
-            'projects/Mrtasty/loaded favorites.png',
-            'projects/Mrtasty/sandwiches.png',
-            'projects/Mrtasty/chicken-and-sides.png',
-            'projects/Mrtasty/hot-dog.png',
+            '/projects/Mrtasty/main.png',
+            '/projects/Mrtasty/loaded favorites.png',
+            '/projects/Mrtasty/sandwiches.png',
+            '/projects/Mrtasty/chicken-and-sides.png',
+            '/projects/Mrtasty/hot-dog.png',
         ],
         summary: 'Restaurant Landing Page & Online Menu',
         description:
